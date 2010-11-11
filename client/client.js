d2575c0ef704b4a5bf7240fbaf3cd84a75e20eec
pelf <pelf@pelfmobile.(none)>
2010-11-11 01:34:17 +0000

    cenas

--- client/client.js
+++ client/client.js
@@ -2,6 +2,7 @@ function process(json) {
 	switch(json.type) {
 case 'msg':
 	// display msg
+	$('chat-wrapper').append('<p><span class="time">[12:00]</span> <a href="http://codebits.eu/intra/s/user/'+json.payload.id+'" class="nick">'+json.payload.name+':</span>'+json.payload.msg+'</p>');
 	break;
 case 'join':
 	// another user joined chat
