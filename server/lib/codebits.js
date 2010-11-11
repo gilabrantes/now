e2e08e0a6460d60b7d29c8e8fb5dbe6f3ac502a6
Tiago Pinto <tpinto@webreakstuff.com>
2010-11-11 00:38:29 +0000

    id for token

--- server/lib/codebits.js
+++ server/lib/codebits.js
@@ -1,3 +1,18 @@
-function auth (token) {
-  
+var sys = require("sys");
+
+function idForToken(token) {
+  var decoded = decodeToken(token);
+  var splat = decoded.split("#");
+  var id = splat[0];
+
+  return id;
 }
+
+function decodeToken(token) {
+  var buf = new Buffer(token,"base64");
+
+  return buf.toString('ascii');
+}
+
+sys.puts(""+idForToken("MjMjdHBpbnRvQHdlYnJlYWtzdHVmZi5jb20jOTUyM2M4NTQyOGUyZTFlODg5Nzg0ZGYxNzE0YWJmM2UjMTI4OTQzMjIyMyM2M2JiMTVhYjk1ZGM1MjYxZjg3NDJmNjVhMTI4YTI5Nw==") );
+
