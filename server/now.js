2c29f4339dc8a7f57ea29068377b11c625201aa8
Tiago Pinto <tpinto@webreakstuff.com>
2010-11-11 01:20:52 +0000

    indentação. require do codebits.js

--- server/now.js
+++ server/now.js
@@ -1,33 +1,34 @@
 var sys = require("sys");
 var net = require("net");
-var ws = require('./lib/node-websocket-server/lib/ws');
+var ws = require('./lib/node-websocket-server/lib/ws.js');
+var codebits = require('./lib/codebits.js');
 
 var server = ws.createServer({
-    debug: true
+	debug: true
 });
 
 // argv parsing
 if (process.argv.length < 4) {
-    sys.puts("Usage: node now.js <domain> <port>");
+	sys.puts("Usage: node now.js <domain> <port>");
 } else {
-    domain = process.argv[2];
-    port = process.argv[3];
-    
-    server.addListener("listening", function(){
-      sys.puts("Codebits now - Listening for connections.");
-      browsers = [];
-    });
+	domain = process.argv[2];
+	port = process.argv[3];
 
-    server.addListener("close", function(conn) {
-            conn.close();
-    });
-
-    server.addListener("connection", function(conn){
-	conn.addListener("message", function(message){
-		sys.puts(message);
-    	//server.broadcast(data);
+	server.addListener("listening", function(){
+		sys.puts("Codebits now - Listening for connections.");
+		browsers = [];
 	});
-    });
+
+	server.addListener("close", function(conn) {
+		conn.close();
+	});
+
+	server.addListener("connection", function(conn){
+		conn.addListener("message", function(message){
+			sys.puts(message);
+			//server.broadcast(data);
+		});
+	});
 
 }
 
