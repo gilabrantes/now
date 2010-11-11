cc60e61c9c69a5166f85550e6d7200103b45098a
pelf <pelf@pelfmobile.(none)>
2010-11-11 14:52:57 +0000

    js changes

--- client/client.js
+++ client/client.js
@@ -1,11 +1,9 @@
 function process(json) {
-	var now = new Date();
-	var h = now.getHours();
-	var m = now.getMinutes();
+var now = new Date();
 	switch(json.type) {
 case 'msg':
 	// display msg
-	$('chat-wrapper').append('<p><span class="time">['+h+':'+m+']</span> <a href="http://codebits.eu/intra/s/user/'+json.payload.id+'" class="nick">'+json.payload.name+':</span>'+json.payload.msg+'</p>');
+	addMsg(now,json.payload.id, json.payload.name, json.payload.msg);
 	break;
 case 'join':
 	// another user joined chat
@@ -22,9 +20,28 @@ default:
 }
 
 function processInput(msg) {
-if (msg.match(/\/auth/)) {
-// api auth
-} else {
-conn.send(msg);
+	if (msg.match(/\/auth/)) {
+		// api auth
+		console.debug('auth!');
+	} else {
+		conn.send(msg);
+	}
 }
+
+function addMsg(now, mid, mname, msg) {
+	var h = now.getHours();
+	var m = now.getMinutes();
+	$('#chat-wrapper').append('<p><span class="time">['+h+':'+m+']</span> <a href="http://codebits.eu/intra/s/user/'+mid+'" class="nick">'+mname+': </a> '+msg+'</p>');
+}
+
+function addInfo(now, msg) {
+	var h = now.getHours();
+	var m = now.getMinutes();
+	$('#chat-wrapper').append('<p class="help"><span class="time">['+h+':'+m+']</span> '+msg+'</p>');
+}
+
+function addSystem(now, msg) {
+	var h = now.getHours();
+	var m = now.getMinutes();
+	$('#chat-wrapper').append('<p class="notice"><span class="time">['+h+':'+m+']</span> '+msg+'</p>');
 }
